cd4b35ae935bfd64c98829660e8d4e7106910bf5
Peter Yoo <trustintofu@gmail.com>
2023-04-18 16:06:12 -0700

    feat(Main): Adds an add new guest button on header, adds a guest table, and adds a guest.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,10 @@
 // -------------------------------- Template ------------------------------- ***
 <template>
   <main
-    class="min-w-screen flex min-h-screen flex-col items-center justify-center bg-eggshell"
+    class="min-w-screen flex min-h-screen flex-col items-center justify-center bg-eggshell px-10"
   >
     <!------------------------------ Header ----------------------------------->
-    <section class="flex w-full max-w-1440 items-center justify-between px-10">
+    <section class="flex w-full max-w-1440 items-center justify-between px-6">
       <div class="flex items-center gap-5">
         <div>
           <img
@@ -12,15 +12,71 @@
             alt="logo"
           />
         </div>
-        <h1 class="text-3xl text-charcoal">NYAN CAT SHOW</h1>
+        <h1 class="text-2xl text-charcoal">NYAN CAT SHOW</h1>
       </div>
       <div>
-        <p class="text-3xl text-charcoal">TOTAL GUESTS: 8</p>
+        <p class="text-2xl text-charcoal">TOTAL GUESTS: 8</p>
       </div>
-      <div>
-        <button class="text-3xl text-charcoal">
+      <div
+        class="group -mr-3 border-2 border-transparent p-0.5 hover:border-2 hover:border-charcoal"
+      >
+        <button
+          class="border border-transparent px-3 py-1 text-2xl text-charcoal group-hover:border group-hover:border-charcoal"
+        >
           ADD NEW GUEST
         </button>
+      </div>
+    </section>
+    <!------------------------------------------------------------------------->
+    <!---------------------------- Guest Table -------------------------------->
+    <section class="mt-1 flex w-full max-w-1440 border-2 border-charcoal p-0.5">
+      <div
+        class="grid h-full w-full grid-cols-2 gap-20 border border-charcoal p-28 text-charcoal"
+      >
+        <!--------------------------- Guest ----------------------------------->
+        <div
+          class="group max-w-350 cursor-pointer border-2 border-transparent p-0.5 hover:border-2 hover:border-charcoal"
+          @mouseenter="isHovered = true"
+          @mouseleave="isHovered = false"
+        >
+          <div
+            class="flex items-center justify-between gap-5 border border-transparent px-6 py-4 group-hover:border group-hover:border-charcoal"
+          >
+            <div class="flex items-center gap-5">
+              <div>
+                <img
+                  src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1681811557/KPA%20Test/girl_xsqr5e.png"
+                  alt="girl"
+                  v-show="!isHovered"
+                />
+                <img
+                  src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1681811352/KPA%20Test/girl-walk_w4vuij.gif"
+                  alt="girl walking"
+                  v-show="isHovered"
+                />
+              </div>
+              <div class="flex flex-col">
+                <p class="text-2xl">FOO@BAR.COM</p>
+                <p class="text-2xl">3 TICKETS</p>
+              </div>
+            </div>
+            <div class="hover:text-charcoal-100">
+              <svg
+                v-if="isHovered"
+                xmlns="http://www.w3.org/2000/svg"
+                width="45"
+                height="45"
+                viewBox="0 0 256 256"
+              >
+                <path
+                  fill="currentColor"
+                  d="M216 48h-40v-8a24 24 0 0 0-24-24h-48a24 24 0 0 0-24 24v8H40a8 8 0 0 0 0 16h8v144a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16V64h8a8 8 0 0 0 0-16ZM96 40a8 8 0 0 1 8-8h48a8 8 0 0 1 8 8v8H96Zm96 168H64V64h128Zm-80-104v64a8 8 0 0 1-16 0v-64a8 8 0 0 1 16 0Zm48 0v64a8 8 0 0 1-16 0v-64a8 8 0 0 1 16 0Z"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <!--------------------------------------------------------------------->
       </div>
     </section>
     <!------------------------------------------------------------------------->
@@ -31,7 +87,9 @@
 <script>
 export default {
   data: () => {
-    return {};
+    return {
+      isHovered: false,
+    };
   },
 };
 </script>
