e840a6a46998bbfbc6ec9b165152461d5f7896f6
Peter Yoo <trustintofu@gmail.com>
2023-04-18 13:03:13 -0700

    feat(tailwind.config.js): Adds custom width, maxWidth, backgroundColor, textColor, fontFamily properties.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,25 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        1440: '1440px',
+      },
+      maxWidth: {
+        1440: '1440px',
+      },
+    },
+    backgroundColor: (theme) => ({
+      ...theme('colors'),
+      eggshell: '#EEE6E1',
+      charcoal: '#565452',
+    }),
+    textColor: {
+      charcoal: '#565452',
+    },
+    fontFamily: {
+      sans: ['VT323', 'Sans-serif'],
+    },
   },
   plugins: [],
 };
