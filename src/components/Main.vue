4de684457ea49ec105e176b996d119b3fbbd0d72
Peter Yoo <trustintofu@gmail.com>
2023-04-18 13:19:13 -0700

    feat(Main): Creates Header section.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,17 +1,38 @@
+// -------------------------------- Template ------------------------------- ***
 <template>
-  <div>
-    <h4>...todo...</h4>
-    <p>Check out the readme file for instructions.</p>
-  </div>
+  <main
+    class="min-w-screen flex min-h-screen flex-col items-center justify-center bg-eggshell"
+  >
+    <!------------------------------ Header ----------------------------------->
+    <section class="flex w-full max-w-1440 items-center justify-between px-10">
+      <div class="flex items-center gap-5">
+        <div>
+          <img
+            src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1681800072/KPA%20Test/logo-nyan_b7u9zp.png"
+            alt="logo"
+          />
+        </div>
+        <h1 class="text-3xl text-charcoal">NYAN CAT SHOW</h1>
+      </div>
+      <div>
+        <p class="text-3xl text-charcoal">TOTAL GUESTS: 8</p>
+      </div>
+      <div>
+        <button class="text-3xl text-charcoal">
+          ADD NEW GUEST
+        </button>
+      </div>
+    </section>
+    <!------------------------------------------------------------------------->
+  </main>
 </template>
-
+// ------------------------------------------------------------------------- ***
+// --------------------------------- Script -------------------------------- ***
 <script>
-  // eslint-disable-next-line no-unused-vars
-  const GuestRepository = require('../guest-repository');
-
-  export default {
-    data: () => {
-      return { };
-    },
-  }
+export default {
+  data: () => {
+    return {};
+  },
+};
 </script>
+// ------------------------------------------------------------------------- ***
