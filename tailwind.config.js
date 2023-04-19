d8b3fac5356b709a432226119acb6d162a526f14
Peter Yoo <trustintofu@gmail.com>
2023-04-18 20:48:15 -0700

    feat(tailwind.config.js): Adds backgroundImage prop, maxWidth prop, maxHeight prop, and backgroundColor prop.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,26 @@ module.exports = {
   content: [],
   theme: {
     extend: {
+      backgroundImage: {
+        'edit-modal-pattern': "url('https://res.cloudinary.com/dryh1nvhk/image/upload/v1681865322/KPA%20Test/Edit%20Modal/bg-tile_udexmd.png')"
+      },
       width: {
         1440: '1440px',
       },
       maxWidth: {
         1440: '1440px',
+        1090: '1090px',
         550: '550px'
       },
+      maxHeight: {
+        650: '650px'
+      }
     },
     backgroundColor: (theme) => ({
       ...theme('colors'),
       eggshell: '#EEE6E1',
       charcoal: '#565452',
+      'charcoal-900': '#33312F'
     }),
     textColor: (theme) => ({
       ...theme('colors'),
