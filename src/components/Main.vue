c5f4b4aa5c051d44b19dc2aac96dbe91c410e3a8
Peter Yoo <trustintofu@gmail.com>
2023-04-18 21:16:46 -0700

    feat(Main): The openEditModal gets the guest data from the Guest component, and then passes the guest data to EditModal, and passes down the updateGuest function.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,7 +6,9 @@
     <EditModal
       v-if="showEditModal"
       @close="showEditModal = false"
-      :show-modal="showEditModal"
+      :showEditModal="showEditModal"
+      :guest="guest"
+      :updateGuest="updateGuest"
     />
     <!------------------------------ HEADER ----------------------------------->
     <section class="flex w-full max-w-1440 items-center justify-between px-6">
@@ -17,16 +19,16 @@
             alt="logo"
           />
         </div>
-        <h1 class="text-2xl text-charcoal">NYAN CAT SHOW</h1>
+        <h1 class="text-3xl text-charcoal">NYAN CAT SHOW</h1>
       </div>
       <div>
-        <p class="text-2xl text-charcoal">TOTAL GUESTS: {{ guests.length }}</p>
+        <p class="text-3xl text-charcoal">TOTAL GUESTS: {{ guests.length }}</p>
       </div>
       <div
         class="group -mr-3 border-2 border-transparent p-0.5 hover:border-2 hover:border-charcoal"
       >
         <button
-          class="border border-transparent px-3 py-1 text-2xl text-charcoal group-hover:border group-hover:border-charcoal"
+          class="border border-transparent px-3 py-1 text-3xl text-charcoal group-hover:border group-hover:border-charcoal"
         >
           ADD NEW GUEST
         </button>
@@ -36,16 +38,15 @@
     <!---------------------------- GUEST TABLE -------------------------------->
     <section class="mt-1 flex w-full max-w-1440 border-2 border-charcoal p-0.5">
       <div
-        class="grid h-full w-full grid-cols-2 place-content-center gap-20 border border-charcoal p-28 text-charcoal"
+        class="grid h-full w-full grid-cols-2 place-content-center gap-5 border border-charcoal p-28 text-charcoal"
       >
         <!-------------------------- GUESTS ----------------------------------->
         <Guest
           v-for="guest in guests"
           :key="guest.email"
           :email="guest.email"
-          :tickets="guest.tickets"
-          :show-edit-modal="false"
-          @show-edit-modal="showEditModal = true"
+          :guest="guest"
+          @open-edit-modal="openEditModal"
         />
         <!--------------------------------------------------------------------->
       </div>
@@ -65,6 +66,7 @@ export default {
     return {
       guests: [],
       showEditModal: false,
+      guest: {},
     };
   },
   components: {
@@ -76,6 +78,15 @@ export default {
     guestRepository.load().then((guests) => {
       this.guests = guests;
     });
+  },
+  methods: {
+    openEditModal(guest) {
+      this.guest = guest;
+      this.showEditModal = true;
+    },
+    updateGuest() {
+      console.log('updateGuest called');
+    },
   },
 };
 </script>
