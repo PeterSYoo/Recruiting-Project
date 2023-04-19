4cbbb8895cb899b0c969ef880c61d3f8679c0391
Peter Yoo <trustintofu@gmail.com>
2023-04-19 16:00:17 -0700

    feat(main.js): Registers VeeValidate, adds ValidationProvider as a global component.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import './index.css';
+import VeeValidate from 'vee-validate';
+import { ValidationProvider } from 'vee-validate';
 
+Vue.use(VeeValidate);
 Vue.config.productionTip = false;
+Vue.component('ValidationProvider', ValidationProvider);
 
 new Vue({
   render: (h) => h(App),
