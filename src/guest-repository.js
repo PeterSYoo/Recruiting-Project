52a11c2ed36c4e414950d04d11893ed7cdbc8378
Peter Yoo <trustintofu@gmail.com>
2023-04-19 21:12:38 -0700

    style(guest-repository.js): Prettier auto formatted.

--- src/guest-repository.js
+++ src/guest-repository.js
@@ -1,4 +1,3 @@
-
 const defaultGuests = [
   { email: 'gangnam-style@psy.com', tickets: 1 },
   { email: 'harlem.shake@baauer.com', tickets: 8 },
@@ -8,24 +7,24 @@ const KEY = 'iscout-recruit-project.guests';
 
 module.exports = class GuestRepository {
   load() {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         try {
           const json = localStorage.getItem(KEY);
           if (!json) throw 'no data found';
           resolve(JSON.parse(json));
-        }catch (e) {
+        } catch (e) {
           resolve(defaultGuests);
         }
       }, 1000);
     });
   }
   save(guests) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         try {
           localStorage.setItem(KEY, JSON.stringify(guests));
-        }catch (e) {
+        } catch (e) {
           // noop
         }
         resolve();
@@ -33,11 +32,11 @@ module.exports = class GuestRepository {
     });
   }
   reset() {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         localStorage.removeItem(KEY);
         resolve();
       }, 1000);
     });
   }
-}
+};
