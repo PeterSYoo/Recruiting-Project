b256092584cedc3c535d6bb846bd454dff694d94
Peter Yoo <trustintofu@gmail.com>
2023-04-20 15:03:07 -0700

    feat(Main): Puts header section into its own section, takes an emitter event from the header component to update the showAddNewGuestModal boolean state.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,6 +3,7 @@
   <main
     class="min-w-screen flex min-h-screen flex-col items-center justify-center bg-eggshell px-10"
   >
+    <!------------------------------ MODALS ----------------------------------->
     <AddNewGuestModal
       v-if="showAddNewGuestModal"
       @close="showAddNewGuestModal = false"
@@ -17,41 +18,12 @@
       :guest="guest"
       :updateGuest="updateGuest"
     />
+    <!------------------------------------------------------------------------->
     <!------------------------------ HEADER ----------------------------------->
-    <section
-      @mouseenter="isHovered = true"
-      @mouseleave="isHovered = false"
-      class="-mb-5 flex w-full max-w-1440 items-center justify-between px-6"
-    >
-      <div class="flex items-center gap-5">
-        <div>
-          <img
-            src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1682026756/KPA%20Test/cat_dgxgcv.png"
-            alt="cat"
-            v-show="!isHovered"
-          />
-          <img
-            src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1682026821/KPA%20Test/cat-animated_brpelg.gif"
-            alt="cat animated"
-            v-show="isHovered"
-          />
-        </div>
-        <h1 class="text-3xl text-charcoal">NYAN CAT SHOW</h1>
-      </div>
-      <div>
-        <p class="text-3xl text-charcoal">TOTAL GUESTS: {{ guests.length }}</p>
-      </div>
-      <div
-        class="group -mr-3 border-2 border-transparent p-0.5 hover:border-2 hover:border-charcoal"
-        @click="showAddNewGuestModal = true"
-      >
-        <button
-          class="border border-transparent px-3 py-1 text-3xl text-charcoal group-hover:border group-hover:border-charcoal"
-        >
-          ADD NEW GUEST
-        </button>
-      </div>
-    </section>
+    <Header
+      :guests="guests"
+      @update-add-new-guest-modal="showAddNewGuestModal = $event"
+    />
     <!------------------------------------------------------------------------->
     <!------------------ GUEST TABLE TOP CORNER DIAMONDS ---------------------->
     <div class="flex w-full max-w-1440 justify-between">
@@ -113,6 +85,7 @@ const GuestRepository = require('../guest-repository');
 import Guest from './Guest.vue';
 import EditGuestModal from './EditGuestModal.vue';
 import AddNewGuestModal from './AddNewGuestModal.vue';
+import Header from './Header.vue';
 
 const guestRepository = new GuestRepository();
 
@@ -121,6 +94,7 @@ export default {
     Guest,
     EditGuestModal,
     AddNewGuestModal,
+    Header,
   },
   data: () => {
     return {
@@ -129,7 +103,6 @@ export default {
       showAddNewGuestModal: false,
       guest: {},
       guestUpdated: false,
-      isHovered: false,
     };
   },
   async mounted() {
