dd5f24b5d910089b65bfa0a48821066ac78e271a
Peter Yoo <trustintofu@gmail.com>
2023-04-20 17:20:13 -0700

    style(AddNewGuestModal): Aligns inputs to the bottom of container height.

--- src/components/AddNewGuestModal.vue
+++ src/components/AddNewGuestModal.vue
@@ -23,7 +23,7 @@
         />
       </div>
       <!----------------------------------------------------------------------->
-      <div class="-ml-10 -mt-10 flex justify-center gap-56">
+      <div class="-ml-10 -mt-10 flex items-end justify-center gap-56">
         <!------------------------ EMAIL INPUT -------------------------------->
         <div class="flex w-1/4 flex-col items-center">
           <div>
